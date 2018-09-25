a01eef009e67c8d3611e0ddf9e66910fff50b631
Dennis Backus <dennisbackus@hotmail.com>
2018-09-25 15:54:25 +0200

    searchbar suggestions toegevoegd

--- service/game.service.ts
+++ service/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { GameDTO } from '../model/GameDTO'; // Dit is onze eigen model
+import { suggestTitleDTO } from '../model/suggestTitleDTO'; // Dit is onze eigen model
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 import 'rxjs/Rx';
@@ -15,11 +15,16 @@ export class GameService {
   constructor(private http: HttpClient ) { }
 
   findGame(title:string): Observable<gameTitleDTO> {
+    console.log(title);
     return this.http.get<gameTitleDTO>('http://localhost:9090/game/scrape/' +title);
   }
 
   findGames(): Observable<gameTitleDTO> {
     return this.http.get<gameTitleDTO>('http://localhost:9090/game/all');
+  }
+
+  getTitles(inputS: string): Observable<suggestTitleDTO> {
+    return this.http.get<suggestTitleDTO>('http://localhost:9090/game/titlesuggest/' + inputS);
   }
 
 }
